93b99219f8911347cc7b9805405995678655f57c
vesagashi1 <vg67863@ubt-uni.net>
2024-04-09 03:11:59 +0200

    Basic elements of the Home Page
    I added two navbars, in the first one there is the 'Logo" and two options for register and login. in the other one we have three more links attached that are home, about us and services. Underneath the navbar i attached a image of a view that is related to the purpose of our platform. I plan on adding more content.

--- booking-app/src/App.js
+++ booking-app/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import viewImage from './images/view.jpg';
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+       <div className="navbar-brand montserrat-alternates-regular">BOOKit</div>
+      <ul className="navbar-nav">
+        {/* <li className="nav-item"><a href="/">Home</a></li>
+        <li className="nav-item"><a href="/about">About</a></li>
+        <li className="nav-item"><a href="/services">Services</a></li> */}
+        <li className="nav-item"><a href="/register">Register</a></li>
+        <li className="nav-item"><a href="/logIn">Log In</a></li>
+      </ul>
+    </nav>
+    
+  );
+};
+
+const SecondNavbar = () =>{
+  return(
+    <div className="second-navbar">
+      <ul className='second-nav'>
+      <li className="nav-items"><a href="/">Home</a></li>
+      <li className="nav-items"><a href="/about">About Us</a></li>
+      <li className="nav-items"><a href="/services">Services</a></li>
+      </ul>
+    </div>
+  );
+};
+
+const ViewImage = () => {
+  return(
+    <div className="image-cont">
+      <img src={viewImage} alt="homeImage" className='fotoja' />
+    </div>
+  );
+};
+
+const UnderImageDiv = () =>{
+  return(
+    <div className="under">
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
+        <SecondNavbar /> 
       </header>
+      <ViewImage />
+      <UnderImageDiv/>{}
     </div>
   );
 }
